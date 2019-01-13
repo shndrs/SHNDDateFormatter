a432a0c526d038a1ff014bea19c353aa64f8d2dc
Sahand Raeisi <sahandraeisi@yahoo.com>
2019-01-13 11:21:58 +0330

    Unit test in on now ;)

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,28 +1,39 @@
 import XCTest
-import SHNDDateFormatter
+import Foundation
+@testable import SHNDDateFormatter
 
 class Tests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    func testSHNDDateFormatter() {
+        
+        let expected = "冬月 26, 0035"
+        
+        let builderObject = DateBuilder { (builder) in
+            builder.inputDateString = "01-01-2019"
+            builder.inputDateFormat = "MM-dd-yyyy"
+            builder.outputDateFormat = "MMM d, yyyy"
+            builder.inputCalenderIdentifier = .gregorian
+            builder.outputCalenderIdentifier = .chinese
+            builder.outputLocale = "zh_Hans_CN"
         }
+        let convertedDate = SHNDDateFormatter(builder: builderObject)?.build()
+        
+        XCTAssertEqual(expected, convertedDate)
+    }
+    
+    func testSHNDNumberFormatter() {
+        
+        let expected = "150,000.45 US dollars"
+        
+        let builderObject = NumberBuilder { (builder) in
+            builder.locale = "en_US"
+            builder.number = NSNumber(value: 150000.45)
+            builder.numberStyle = .currencyPlural
+        }
+        
+        let convertedNumber = SHNDNumberFormatter(builder: builderObject)?.convert()
+        
+        XCTAssertEqual(expected, convertedNumber)
     }
     
 }
